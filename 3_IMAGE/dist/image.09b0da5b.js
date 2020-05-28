ecb37a3a6238a0bb02128a185cfcc131accf58d1
HongNyungKi <hnk2017@naver.com>
2020-05-29 00:00:25 +0900

    [Add]finish

--- 3_IMAGE/dist/image.09b0da5b.js
+++ 3_IMAGE/dist/image.09b0da5b.js
@@ -118,7 +118,80 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"image.js":[function(require,module,exports) {
+var $play = $('.play'),
+    $detail = $('.detail'),
+    $movie = $('.movie', $detail),
+    $close = $('.close');
+$('.movies .movie').click(function () {
+  $movie.html($(this).html());
+  $play.appendTo($movie);
+  $poster = $('.poster', this).addClass('active');
+  $('.poster', $detail).css({
+    top: $poster.position().top,
+    left: $poster.position().left,
+    width: $poster.width(),
+    height: $poster.height()
+  }).data({
+    top: $poster.position().top,
+    left: $poster.position().left,
+    width: $poster.width(),
+    height: $poster.height()
+  });
+  $detail.show();
+  $('.poster', $detail).delay(10).queue(function (next) {
+    $detail.addClass('ready');
+    next();
+  }).delay(100).queue(function (next) {
+    $(this).css({
+      top: '-10%',
+      left: '-6%',
+      width: 266,
+      height: 400
+    });
+    next();
+  });
+});
+/*--------------------
+Close
+--------------------*/
 
+function close() {
+  console.log('asd');
+  $p = $('.detail .poster');
+  console.log($p);
+  $p.css({
+    top: $p.data('top'),
+    left: $p.data('left'),
+    width: $p.data('width'),
+    height: $p.data('height')
+  });
+  $detail.removeClass('ready').delay(500).queue(function (next) {
+    $(this).hide();
+    $poster.removeClass('active');
+    next();
+  });
+}
+
+$close.click(close);
+$('body').click(function (e) {
+  $p = $(e.target).parents();
+
+  if ($p.is('.app')) {
+    return false;
+  } else {
+    close();
+  }
+});
+/*--------------------
+CodePen Thumbnail
+--------------------*/
+
+setTimeout(function () {
+  $('.movie:eq(0)').click();
+}, 300);
+setTimeout(function () {
+  close();
+}, 1700);
 },{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -147,7 +220,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64819" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64858" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
