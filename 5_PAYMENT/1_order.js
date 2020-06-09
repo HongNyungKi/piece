80961498fd07a3b7793d4d803258d83f7d19e8b7
HongNyungKi <hnk2017@naver.com>
2020-06-09 20:27:17 +0900

    [Add]order page

---
+++ 5_PAYMENT/1_order.js
@@ -0,0 +1,37 @@
+$(function () {
+    var $selectEmail = $(".select_domain"),
+        $targetInput = $(".email_domain");
+
+    $selectEmail.change(function () {
+        var domain = $(this).val();
+        $targetInput.val(domain);
+    });
+
+    var $count = $(".count"),
+        $unit = $(".unitPrice").text()
+    $unitPrice = $unit.replace(",", ""),
+        //속성을 사용하여 포현해볼것
+        $currentNumber = parseInt($count.text()),
+        $total = $(".total");
+
+
+
+
+    $("span a").click(function (e) {
+        e.preventDefault();
+        if ($(this).hasClass("plus")) {
+            $currentNumber += 1;
+        } else {
+            if ($currentNumber > 0) {
+                $currentNumber -= 1;
+            }
+        }
+
+        $count.text($currentNumber);
+
+        var semiTotal = $unitPrice * $currentNumber;
+        var total = Number(semiTotal).toLocaleString("en");
+        $total.text(total);
+
+    })
+});
